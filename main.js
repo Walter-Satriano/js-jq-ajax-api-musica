360ebd2a5350926d0d17bb7daf302f6d288a8fe0
Walter Satriano <walter.satri@gmail.com>
2019-07-30 18:14:25 +0200

    inserita chiamata ajax

---
+++ main.js
@@ -0,0 +1,43 @@
+/*ESERCIZIO:
+Attraverso una chiamata ajax all’Api di boolean avremo
+a disposizione una decina di dischi musicali.
+Servendoci di handlebars stampiamo tutto a schermo.*/
+
+$(document).ready(function() {
+
+  var musicApi = "https://flynn.boolean.careers/exercises/api/array/music";
+
+  // chiamata ajax per richiedere la lista di cd musicali
+  $.ajax(
+    {
+      url : musicApi,
+      method : "GET",
+      success : function(data) {
+
+        var cdItem = data.response;
+        console.log(cdItem);
+
+        // ciclo per poter iterare l'array di oggetti ricevuto dalla chiamata ajax
+        for (var i = 0; i < cdItem.length; i++) {
+
+          //compilo il template
+          var source = $("#cd_template").html();
+          var template = Handlebars.compile(source);
+
+          //assegno ai segnaposto scelti nell'html l'elemento corretto
+          var context = {immagine: cdItem[i].poster , titoloAlbum: cdItem[i].title , autore: cdItem[i].author , anno: cdItem[i].year};
+          console.log("questo è il context " , context);
+          var html = template(context);
+
+          //stampo a schermo
+          $(".cds-container").append(html);
+        }
+      },
+      error : function(richiesta, stato, errore) {
+        console.log("E' avvenuto un errore. " + errore);
+      }
+
+    }
+  );
+
+});
